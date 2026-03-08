7902944c3e8a66dd2e063a50bd195abbf2e5f09c
TheCoonpany <info@thecoonpany.dev>
2026-03-08 13:47:26 +0100

    refactor: better approach for home layout

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,11 +1,18 @@
+import type { AppTextInput } from './elements/TextInput';
+
 declare namespace astroHTML.JSX {
   interface HTMLAttributes {
-    // "data-count"?: number;
+    /** Attr */
+    "my-count"?: number;
     // "data-label"?: string;
   }
 
   // Add a CSS custom property to the style object
   interface CSSProperties {
     // "--theme-color"?: "black" | "white";
+  }
+
+  interface HTMLElementTagNameMap {
+    ['app-text-input']: AppTextInput;
   }
 }
